58b16839444341d3424c7682f55e97883f5eac12
nikolas <nikola.stojilkovic6@gmail.com>
2023-01-08 20:06:59 +0100

    HomePage component added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css"
 import NavigationBar from "./components/NavigationBar"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "./components/HomePage"
 
 function App() {
   return (
     <BrowserRouter>
       <NavigationBar></NavigationBar>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "../css/HomePage.css"
+
+function HomePage() {
+  return (
+    <div className="page">
+      <div className="picture">
+        <h1>Welcome to Recipe Book</h1>
+        <h2>Search for desired recipes</h2>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
